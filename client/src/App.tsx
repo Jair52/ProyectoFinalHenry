935053d17bc7eb738e356e1c9d9cde7b9ed11b92
Mirko1523 <134454433+Mirko1523@users.noreply.github.com>
2024-03-20 23:03:25 -0300

    NuestrosPlatos
    Hice arreglos css de navBar, cree las paginas y rutas de los botones navbar y no mucho mas.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,28 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
+import NavBar from './components/NavBar/NavBar.tsx';
+import Home from './components/Home/Home';
+import NuestrosPlatos from './components/NuestrosPlatos/Platos.tsx';
+import Funciona from './components/ComoFunciona/Funciona.tsx';
+import QuienesSomos from './components/QuienesSomos/QuienesSomos.tsx';
+import Faqs from './components/FAQ\'S/Faqs.tsx';
+
 
 function App() {
 
   return (
     <>
       <div>
+        <NavBar onItemClick={()=>{}}/>
         <Routes>
-          <Route path="/home" />
+          <Route path="/" element={<Home/>}/>
           <Route path="/form" />
           <Route path="/detail/:id" />
           <Route path="*" />
+          <Route path="/NuestrosPlatos" element={<NuestrosPlatos/>}/>
+          <Route path="/Comofunciona" element={<Funciona/>}/>
+          <Route path="/QuienesSomos" element={<QuienesSomos/>}/>
+          <Route path="/Faqs" element={<Faqs/>}/>
         </Routes>
       </div>
     </>
@@ -18,4 +30,3 @@ function App() {
 }
 
 export default App;
-
---
+++ client/src/components/NuestrosPlatos/Platos.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+interface PlatoProps {
+
+}
+
+const NuestrosPlatos: React.FC<PlatoProps> = () => {
+  return (
+   
+    <div>
+      <h1>HOLAAAAA</h1>
+    </div>
+    
+    
+  );
+}
+
+export default NuestrosPlatos;
