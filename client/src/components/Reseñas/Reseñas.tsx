42f98c97c1d53aeb63a2208d53a8447cab14cb28
Gaston Vergagni <116928445+Gastonvg@users.noreply.github.com>
2024-03-25 20:32:03 -0300

    reseñas
    reseñas

--- client/src/components/Reseñas/Reseñas.tsx
+++ client/src/components/Reseñas/Reseñas.tsx
@@ -12,24 +12,28 @@ const Reseñas: React.FC = () =>{
                           <div className={styles.cartasreseñas2}>
                             <div className={styles.cartasreseñas}>
                                 <div className={styles.reseña}>
+                                    <p className={styles.textoreseñaestrella}>★★★★★</p>
                                     <p className={styles.textoreseña}>Pedi la bandeja paisa y me enamore</p>
                                     <p className={styles.textonombre}>Aurelio Cabello</p>
                                 </div >
                             </div>
                             <div className={styles.cartasreseñas}>
                                 <div className={styles.reseña}>
+                                <p className={styles.textoreseñaestrella}>★★★★★</p>
                                     <p className={styles.textoreseña}>Me pedi 40 milanesas y no me arrepiento de nada, estaban muy ricas</p>
                                     <p className={styles.textonombre}>Joel Fernandez</p>
                                 </div >
                             </div>
                             <div className={styles.cartasreseñas}>
                                 <div className={styles.reseña}>
+                                <p className={styles.textoreseñaestrella}>★★★★★</p>
                                     <p className={styles.textoreseña}>Excelente los platos, todo estaba  en su punto. la atencion fue muy buena en todo momento... recomendado</p>
                                     <p className={styles.textonombre}>Sharon Gonzalez</p>
                                 </div >
                             </div>
                             <div className={styles.cartasreseñas}>
                                 <div className={styles.reseña}>
+                                    <p className={styles.textoreseñaestrella}>★★★★★</p>
                                     <p className={styles.textoreseña}>Todo muy rico, me pedi comida para toda la semana</p>
                                     <p className={styles.textonombre}>Joel Fernandez</p>
                                 </div >
